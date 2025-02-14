a43918b5647375650df93cf612f5c121e6cfd67f
Zubrungus <58639394+Zubrungus@users.noreply.github.com>
2025-02-13 23:32:56 -0600

    Work on client side auth api

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,7 +1,23 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try{
+    const response = await fetch('auth/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(userInfo),
+    });
+
+    const data = await response.json();
+    if(!response.ok){
+      console.error('Error, check network tab');
+    }
+
+    return data;
+  } catch(err){
+    console.error(`Error: ${err}`);
+    return [];
+  }
 }
 
 
